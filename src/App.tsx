ca27d6a82e073a454933da1d33a03f4a84ecdd38
michelbr84 <166889728+michelbr84@users.noreply.github.com>
2025-06-03 15:03:50 -0400

    Start

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// src/App.tsx
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <p>Welcome to your first React + TypeScript page.</p>
+    </div>
+  );
+}
+
+export default App;
